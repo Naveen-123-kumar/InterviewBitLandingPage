fc5aa956f979e08ef95902449ffeb92a411b7b64
Naveen Kumar <1998naveenk@gmail.com>
2022-03-01 17:34:46 +0530

    Commit InterviewBitPage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { createTheme,responsiveFontSizes,ThemeProvider } from '@mui/material';
+import React from 'react';
+import DreamJob from './Components/DreamJob';
+import FooterComponent1 from './Components/Footer/FooterComponent1';
+import PaperComponent from './Components/PracticeProblem/PaperComponent';
+import RadioCom from './Components/RadioComponent/RadioCom';
+import ScallerAcadmy from './Components/ScallerAcadmy/ScallerAcadmy';
+import TechInterview from './Components/TechInterview/TechInterview';
+import VideoExplaination from './Components/VideoExplaination';
+import BottomScroll from './Pages/BottomScroll';
+import CodingEnviromentPage from './Pages/CodingEnviromentPage';
+import FastCoursesPage from './Pages/FastCoursesPage';
+import ImagePage from './Pages/ImagePage';
+import MockInterviewPage from './Pages/MockInterviewPage';
+import Navbar from './Pages/Navbar';
+import TestimonialPage from './Pages/TestimonialPage';
+const Theme =responsiveFontSizes(createTheme());
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={Theme}>  
+      <Navbar/>
+      
+      <TechInterview/>
+      {/* <BottomScroll/> */}
+      {/* <RadioCom/> */}
+      {/* <ImagePage/> */}
+      <TestimonialPage/>
+      <ScallerAcadmy/>
+      <PaperComponent/>
+      <FastCoursesPage/>  
+      <MockInterviewPage/>
+       <VideoExplaination/>
+       <CodingEnviromentPage/>
+      <DreamJob/>
+      <FooterComponent1/> 
+    </ThemeProvider> 
+  )
 }
 
-export default App;
+export default App
